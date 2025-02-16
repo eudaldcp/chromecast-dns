cdfb7e92d42e4874fa917af4732c32839a1d02dc
Your Name <you@example.com>
2025-02-16 10:57:29 +0100

    Clean code

--- chromecast-dns.sh
+++ chromecast-dns.sh
@@ -3,10 +3,8 @@
 [ -z "$1" ] && echo "Usage: ${0##*/} CHROMECAST_IP [DNS_over_TCP_SERVER]" && exit 1
 DNS_SERVER="${2:-base.dns.mullvad.net}"
 
-# Ensure ADB server is running and suppress all output
 adb devices &>/dev/null
 
-# Try connecting until successful, suppress unnecessary output
 while ! adb connect "$1" 2>&1 | grep -q "connected to"; do
     echo "Waiting for device to be available..."
     sleep 1
